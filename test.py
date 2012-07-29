0434c1961618628c9db0faa049b94d476f7a1f6a
Sebastian Benthall <sbenthall@gmail.com>
2012-07-29 16:40:53 -0400

    a new test for the trailing period tokenization issue

--- test.py
+++ test.py
@@ -48,6 +48,9 @@ class GeneralTests(unittest.TestCase):
         print "Knowledge base documents"
         print self.text1
         print self.text2
+        print "Knowledge base relations"
+        print self.kb.relations
+
 
         print "Query document 1"
         self.text3 = "Today was a good day because I ate ice cream."
@@ -68,6 +71,7 @@ class GeneralTests(unittest.TestCase):
         self.dox5 = parse.Document(self.text5).to_dox()
         print self.text5
         print "Consistency score:", self.kb.query(self.dox5)
+        self.assertTrue(parse.Relation(True,'Today','good') in self.kb.query(self.dox5)[1])
 
 if __name__ == '__main__':
     unittest.main()
