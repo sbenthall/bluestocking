eeed21421d4eb8540719612cf203ae3e4ab461a3
Sebastian Benthall <sbenthall@gmail.com>
2012-08-25 19:55:06 -0700

    some changes required for packaging.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='bluestocking',
+    version='0.1dev',
+    packages=['bluestocking'],
+    license='GPL v3'
+    long_description=open('README.md').read()
+)
