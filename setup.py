50d322af4a8a493ece09f305ee003adde88f849e
Sebastian Benthall <sbenthall@gmail.com>
2012-09-16 21:27:51 -0700

    touching up setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='bluestocking',
-    version='0.1.1',
+    version='0.1.2',
     author_email='bluestocking-dev@googlegroups.com',
-    packages=['bluestocking'],
+    packages=find_packages(), #['bluestocking'],
     license='GPL',
     description="An information extraction toolkit built on top of NLTK.",
     long_description=open('README.md').read(),
-    install_requires=['nltk']
+    install_requires=['nltk','numpy']
 )
